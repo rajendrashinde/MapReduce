a609513f7a2c3a46a4ebb41adcb3d19b2d145210
Rajendra Shinde <rajshind@cisco.com>
2012-05-12 15:36:01 -0700

    Parameters

--- lsh/src/Parameters.java
+++ lsh/src/Parameters.java
@@ -1,9 +1,9 @@
 package lsh; 
 public class Parameters {
-	double W = 1.0; 
-	int k = 8; 
+	double W = 0.5; 
+	int k = 16; 
 	int d = 549532; 
-	double u = 0.75; 
+	double u = 0.5; 
 	double c = 2.0; 
-	int L = 10; 
+	int L = 100; 
 double D = 0.25; }
